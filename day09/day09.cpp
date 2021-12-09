8af610a3271795b52fb3a96cf21ad86731c35110
rytc <rytc@protonmail.com>
2021-12-09 07:14:10 -0800

    day09 part 1

--- day09/day09.cpp
+++ day09/day09.cpp
@@ -5,15 +5,68 @@ int main(int argc, char** argv) {
 
     u32 rowCount = count_newlines(input);
     u32 colCount = find_next(input, '\n');
-    u8* heightmap = (u8*)malloc((rowCount*colCount)*sizeof(u8));
 
-    for(u32 i = 0; i < input.length; i++) {
-        if(input.data[i] == '\n') continue;
-        heightmap[i] = input.data[i];
+    s8* heightmap = (s8*)malloc((rowCount*colCount)*sizeof(s8));
+
+    u32 index = 0;
+    for(u32 y = 0; y < rowCount;) {
+        for(u32 x = 0; x < colCount;) {
+            if(input.data[index] == '\n') { 
+                index++;
+                continue;
+            }
+            heightmap[x + (y*colCount)] = input.data[index] - 48;
+            x++;
+            index++;
+        }
+        y++;
     }
 
-    printf("Last number is %i\n", heightmap[(rowCount*colCount) - 1]);
+    // Map visualization
+    #if 0
+    for(u32 y = 0; y < rowCount; y++) {
+        for(u32 x = 0; x < colCount; x++) {
+            printf("%i", heightmap[x + (y*(colCount))]);
+        }
+        printf("\n");
+    }
+    #endif
 
+    u32 riskLevels = 0;
+
+    for(u32 y = 0; y < rowCount; y++) {
+        for(u32 x = 0; x < colCount; x++) {
+            u32 thisHeight = heightmap[x + (y*colCount)];
+
+            s8 neighbors[4] = {-1, -1, -1, -1};
+            
+            if(y > 0)          neighbors[0] = heightmap[(x+0) + ((y-1) * colCount)];
+            if(x > 0)          neighbors[1] = heightmap[(x-1) + (y * colCount)];
+            if(x < colCount-1) neighbors[2] = heightmap[(x+1) + (y * colCount)];
+            if(y < rowCount-1) neighbors[3] = heightmap[(x+0) + ((y+1) * colCount)];
+
+
+            b32 isLowPoint = true;
+            for(u32 i = 0; i < 4; i++) {
+                if(neighbors[i] >= 0 && neighbors[i] <= thisHeight) {
+                    isLowPoint = false;
+                    break;
+                }
+            }
+
+            if(isLowPoint) {
+                #if 0
+                printf("  %i  \n", neighbors[0]);
+                printf("%i %i %i\n", neighbors[1], thisHeight, neighbors[2]);
+                printf("  %i  \n\n", neighbors[3]);
+                #endif
+                
+                riskLevels += thisHeight+1;
+            }
+        }
+    }
+
+    printf("\nSum of risk levels: %i\n", riskLevels);
 
     free_file(input);
     return 0;
