e911567fd84ef6a5a82d725641b7f96e017de0df
rytc <rytc@protonmail.com>
2021-12-15 08:15:01 -0800

    Day14 hashmap attempt

--- day14/day14.cpp
+++ day14/day14.cpp
@@ -1,5 +1,7 @@
 #include "../utils/utils.h"
 #include <cassert>
+#include <unordered_map>
+
 
 struct Rule {
     char match[2];
@@ -10,6 +12,7 @@ struct Rule {
 #define MB(v) (KB(v)*1024L)
 
 constexpr u64 BUFFER_SIZE = MB(1024);
+constexpr u32 ASCII_OFFSET = 65; 
 
 // I think this is UB or source of slowness
 // compiler/cpu needs checks to makes sure srcbuffer and dstBuffer are not at the same location
@@ -51,6 +54,82 @@ p1_insertion(u8* srcBuffer, u8* dstBuffer, u32 srcCount, Rule* rules, u32 ruleCo
     assert(writeCursor > 0);
     return writeCursor;
 }
+
+static u32
+p2_hash(char a, char b) {
+    return (u32)a + ((u32)b*1000);
+}
+
+static u64
+p2_insertion(u8* srcBuffer, u32 count, Rule* rules, u32 ruleCount) {
+    u64 result = 0;
+
+    std::unordered_map<u32, s64> maps;
+
+    for(u32 i = 0; i < count-1; i++) {
+        maps[p2_hash(srcBuffer[i], srcBuffer[i+1])] = 1;
+    }
+
+    u32 dst = 1;
+    u32 src = 0;
+    for(u32 step = 0; step < 10; step++) {
+        printf("Step %i\n", step+1);
+        Rule* lastMatch = nullptr;
+
+        for(auto itr = maps.begin(); itr != maps.end(); itr++) {
+            u8 second = itr->first / 1000;
+            u8 first = itr->first - (second * 1000);
+
+            if(lastMatch) {
+                u32 newPair3 = p2_hash(lastMatch->match[1], first);
+                maps[newPair3] += 1;
+                lastMatch = nullptr;
+            }
+            
+            b32 ruleMatch = false;
+            for(u32 ruleIndex = 0; ruleIndex < ruleCount; ruleIndex++) {
+                Rule rule = rules[ruleIndex];
+                if(rule.match[0] == first && rule.match[1] == second) {
+                    u32 newPair = p2_hash(rule.match[0], rule.insert);
+                    u32 newPair2 = p2_hash(rule.insert, rule.match[1]);
+                    maps[p2_hash(first, second)] -= 1;
+                    maps[newPair] += 1;
+                    maps[newPair2] += 1;
+                    
+                    lastMatch = &rules[ruleIndex];
+
+                    ruleMatch = true;
+                    break;
+                }
+            }
+        }
+    }
+
+    printf("Part 2 pair count: %llu\n", maps.size());
+    u64 minElements = UINT64_MAX;
+    u64 maxElements = 0;
+
+    s64 counts[26] = {};
+
+    for(auto itr = maps.begin(); itr != maps.end(); itr++) {
+        u8 second = itr->first / 1000;
+        u8 first = itr->first - (second * 1000);
+        counts[first - ASCII_OFFSET] += itr->second;
+        counts[second - ASCII_OFFSET] += itr->second;
+    }
+
+    for(u32 i = 0; i < 26; i++) {
+        maxElements = MAX(maxElements, counts[i]);
+        if(counts[i] > 0) minElements = MIN(minElements, counts[i]);
+    }
+
+    printf("Part 2 Most common element count: %llu\n", maxElements);
+    printf("Part 2 Least common element count: %llu\n", minElements);
+
+    return maxElements - minElements;
+}
+
+
 
 int main(int argc, char** argv) {
     File input = read_entire_file("input.txt");
@@ -100,7 +179,7 @@ int main(int argc, char** argv) {
         // Count the elements
         for(u32 i = 0; i < p1SrcCount; i++) {
             // src is the last buffer that was written to
-            elementCounts[src[i] - 65] += 1;
+            elementCounts[src[i] - ASCII_OFFSET] += 1;
         }
 
         for(u32 e = 0; e < 26; e++) {
@@ -111,54 +190,19 @@ int main(int argc, char** argv) {
     }
 
     // Part 2
-    #if 0 // Will come back to this. 40 steps is too many to brute force
-    u64 p2MaxElement = 0;
-    u64 p2MinElement = 99999;
-    u64 p2SrcCount = 20;
+    u64 p2Difference = 0;
     {
         memcpy(bufferA, input.data, 20);
-        bufferA[20] = '\0';
-        u8* src = bufferA;
-        u8* dst = bufferB;
-        u64 srcCount = 20;
-
-        printf("Template: %s\n", src);
-        for(u32 i = 0; i < 40; i++) {
-            p2SrcCount = p1_insertion(src, dst, p2SrcCount, rules, ruleCount);
-            printf("Step %i\n", i+1, dst);
-            u8* swap = src;
-            src = dst;
-            dst = swap;
-        }
-
-        u64 elementCounts[26] = {};
-
-        // Count the elements
-        for(u64 i = 0; i < p2SrcCount; i++) {
-            // src is the last buffer that was written to
-            elementCounts[src[i] - 65] += 1;
-        }
-
-        for(u32 e = 0; e < 26; e++) {
-            p2MaxElement = MAX(p2MaxElement, elementCounts[e]);
-            // not every alphabet character is used
-            if(elementCounts[e] > 0) p2MinElement = MIN(p2MinElement, elementCounts[e]);
-        }
+        p2Difference = p2_insertion(bufferA, 20, rules, ruleCount);
     }
-    #endif 
 
     printf("------------\n");
     printf("Part 1 count: %i\n", p1SrcCount);
     printf("Part 1 Most common element count: %i\n", p1MaxElement);
     printf("Part 1 Least common element count: %i\n", p1MinElement);
     printf("Part 1 result: %i\n", p1MaxElement - p1MinElement);
+    printf("Part 2 result: %llu\n", p2Difference);
 
-    #if 0
-    printf("Part 2 count: %llu\n", p2SrcCount);
-    printf("Part 2 Most common element count: %llu\n", p2MaxElement);
-    printf("Part 2 Least common element count: %llu\n", p2MinElement);
-    printf("Part 2 result: %llu\n", p2MaxElement - p2MinElement);
-    #endif
 
     
     free(bufferA); bufferB = nullptr;
